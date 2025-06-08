3bf3b9985b7495cc28a954ef569a12995815ad46
Seanachan <hsinchen.pai1218@gmail.com>
2025-06-08 20:03:55 +0800

    fix: accept url from frontend

--- backend/src/main/java/org/kungnection/backend/config/WebConfig.java
+++ backend/src/main/java/org/kungnection/backend/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://kungnection-chat-4bbu6jlfu-seanachans-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
